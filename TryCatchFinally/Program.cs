e6d097db880c8791a5038dae6aa9c4165f2b7ec0
Adam Macken <mackenadam@gmail.com>
2021-02-20 21:40:33 +1300

    Add argumentNull exception

--- TryCatchFinally/Program.cs
+++ TryCatchFinally/Program.cs
@@ -16,11 +16,15 @@ namespace TryCatchFinally
             }
             catch (FormatException)
             {
-                Console.WriteLine("Format exception, please enter a number.");
+                Console.WriteLine("Format exception, please make sure your input is a number.");
             }
             catch (OverflowException)
             {
-                Console.WriteLine("Overflow exception, your number is too large.");
+                Console.WriteLine("Overflow exception, your number is too large or too small for an Int32.");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Argument Null exception, make sure you have input a number.");
             }
 
         }
